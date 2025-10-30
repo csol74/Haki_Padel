1976a13ffa52690eab590c17ac77bec4b2140265
csol74 <168322590+csol74@users.noreply.github.com>
2025-10-29 21:50:49 -0500

    Arreglo de ruta para iniciar la web en login

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\InicioController;
 
 Auth::routes();
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/', [HomeController::class, 'welcome']);
 
